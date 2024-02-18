3a10e0ff885b00c1d9c27709e691e0c8ddf2a1e7
Bukharova Tatyana <tatyana.bu@inbox.ru>
2024-02-18 17:42:57 +0300

    Task 2 added

---
+++ L1Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 2;
+int b = 5;
+int c = 8;
+int d = 4;
+int e = 9;
+int max = a;
+
+if(max < b)
+{
+    max = b;
+}
+if(max < c)
+{
+    max = c;
+}
+if(max < d)
+{
+    max = d;
+}
+if(max < e)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
