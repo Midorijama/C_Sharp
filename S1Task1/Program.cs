e143c9dc2ccf9dd05a13714c8c71aa3004255875
Bukharova Tatyana <tatyana.bu@inbox.ru>
2024-02-19 13:38:01 +0300

    Solution from seminar added

--- S1Task1/Program.cs
+++ S1Task1/Program.cs
@@ -10,3 +10,18 @@ else
 {
     Console.WriteLine("No");
 }
+
+//Seminar solution
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a == b*b)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
