c51d649cffaf0e77a886d1ca5062c4e6a46a548e
Bukharova Tatyana <tatyana.bu@inbox.ru>
2024-02-19 15:27:55 +0300

    First homework second task passed

---
+++ HW1Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int result = a;
+
+if(result < b)
+{
+    result = b;
+}
+if(result < c)
+{
+    result = c;
+}
+Console.WriteLine(result);
