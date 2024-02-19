330315aa64c53f791424853a47d24c30d0edc61b
Bukharova Tatyana <tatyana.bu@inbox.ru>
2024-02-19 14:46:17 +0300

    Seminar work added

---
+++ Seminar1Work/Program.cs
@@ -0,0 +1,25 @@
+﻿//Task 1
+// Console.Write("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a == Math.Pow(b,2))
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+
+//Task 2
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = -n;
+while(i <= n)
+{
+    Console.Write(i);
+    Console.Write(" ");
+    i = i + 1;
+}
---
+++ Task3Showcase/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 100;
+int c = a % 10;
+int sum = b + c;
+Console.WriteLine(sum);
