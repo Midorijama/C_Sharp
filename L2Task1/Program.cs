316164c626102a99e95122ac51d588233322e9eb
Bukharova Tatyana <tatyana.bu@inbox.ru>
2024-02-21 23:10:20 +0300

    First task for lecture two added

---
+++ L2Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = new int[5]; // Создание массива на 5 целых чисел; тип массив название присваивание выделение памяти под тип и количество элементов
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 3;
+arr[3] = 6;
+arr[4] = 1;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5] { 4, 6, 9, 3, 2 };
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
