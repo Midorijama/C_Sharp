f42f9e5806b186ebb2a08f0623b38c8f222608bb
Bukharova Tatyana <tatyana.bu@inbox.ru>
2024-02-22 01:31:20 +0300

    Finalized task4

--- S3Task4/Program.cs
+++ S3Task4/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите трехзначное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 int[] arr = new int[3];
-int d = a;
+int d = 1;
 if (a < 100 && a > 999)
 {
     Console.WriteLine("Число не трехзначное");
@@ -10,10 +10,13 @@ else
 {
     for (int i = 0; i < 3; i++)
     {
- 
+        int n = a/d%10;
+        arr[i] = n;
+        d = d*10; 
     }
 }
 foreach (int e in arr)
 {
     Console.Write($"{e}, ");
 }
+Console.Write("\b\b  ");
